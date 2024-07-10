e93268e6c5783dbb20511b8e86415c48e5cc2118
aughade <akashughade77@gmail.com>
2024-07-10 18:10:45 +0530

    feature added updatecli

---
+++ input.tf
@@ -0,0 +1,31 @@
+variable "cluster-autoscaler" {
+ default = "9.21.0"
+}
+
+variable "ingress-nginx" {
+ default = "4.3.0"
+}
+
+variable "metrics-server" {
+ default = "3.8.2"
+}
+
+variable "aws-efs-csi-driver" {
+ default = "2.2.9"
+}
+
+variable "aws-load-balancer-controller" {
+ default = "1.5.5"
+}
+
+variable "cert-manager" {
+ default = "v1.10.0"
+}
+
+variable "datadog-crds" {
+ default = "0.4.7"
+}
+
+variable "datadog" {
+ default = "3.1.11"
+}
